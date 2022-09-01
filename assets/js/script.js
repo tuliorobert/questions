74c52bddc9d96c9df6feb6e7d0d35a8d01492f48
Tulio Roberto <tuliorobert2@gmail.com>
2022-09-01 11:48:17 -0300

    Add files

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+function sortAnswer() {
+    let answers = [
+        "Definitivamente sim!",
+        "Acho que sim",
+        "Talvez...",
+        "Acredito que não :(",
+        "De jeito nenhum!",
+        "Na real, eu não me importo"
+    ]
+
+    return answers[parseInt(Math.random() * (answers.length - 1))];
+}
+
+addEventListener("keypress", function(e) {
+    if(e.key == "Enter") {
+        getAnswer();
+    }
+});
+
+function getAnswer() {
+    let questionArea = document.getElementById("question");
+    let answerArea = document.getElementById("answer");
+
+    let lastChar = String(questionArea.value).charAt((questionArea.value).length - 1);
+
+    if(questionArea.value != "") {
+        if(lastChar == "?") {
+            answerArea.classList.remove("answer-finally");
+            answerArea.classList.add("answer-thinking");
+            answerArea.textContent = "Pensando...";
+    
+            setTimeout(() => {
+                answerArea.classList.remove("answer-thinking");
+                answerArea.classList.add("answer-finally");
+                answerArea.textContent = sortAnswer();
+            }, 2000);
+    
+            questionArea.value = "";
+        } else alert("Se sua pergunta não tiver um '?' ela se torna um afimação, tente de novo!");
+
+    } else alert("Escreva uma pergunta primeiro!");
+}
